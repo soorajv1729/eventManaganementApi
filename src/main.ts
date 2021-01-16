c509749d3ac6f885b21af90999b7a66de34cf476
sooraj <sooraj@revyrieglobal.com>
2021-01-15 19:25:20 -0800

    initial setups

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Event Management Api')
+    .setDescription('Apis for Event Management app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+  Logger.log(`Server running on http://localhost:${3000}`, 'Bootstrap');
+}
+bootstrap();
